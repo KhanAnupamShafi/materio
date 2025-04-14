e2f99e84d1cd20b2ae203615a9b9bd0a822ad137
KhanAnupamShafi <deep71.bd@gmail.com>
2025-04-15 00:07:10 +0600

    fix next config

--- javascript-version/next.config.js
+++ javascript-version/next.config.js
@@ -3,6 +3,8 @@ const path = require('path')
 module.exports = {
   trailingSlash: true,
   reactStrictMode: false,
+  basePath: '/materio',
+  assetPrefix: '/materio/',
   experimental: {
     esmExternals: false,
     jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
